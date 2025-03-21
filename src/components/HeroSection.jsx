2b6ff61b707af01b9d259a39a62975e632324b83
ignatgft <gorborukovignst@gmail.com>
2025-03-21 14:20:38 +0600

    added final hero section in frontend

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/HeroSection.css';
+import heroSection from '../assets/heroSection.svg'
+
+
+const HeroSection = () => {
+  return (
+    <section className="hero-section">
+      <div className="hero-content poppins-bold">
+        {/* Текстовая часть */}
+        <h1 className="hero-title">
+          Studying Online is <br/>now much easier.
+        </h1>
+        <p className="hero-subtitle poppins-regular">
+          Okututor is an interesting platform that will <br /> teach you in more interactive way
+        </p>
+
+        {/* Блок с текстом и кнопкой */}
+        <div className="hero-buttons inter">
+          <div className="find-tutor-text">Find your tutor</div>
+          <Link to="/start" className="btn start-btn">
+            Start
+          </Link>
+        </div>
+      </div>
+
+      {/* Изображение с иконками */}
+      <div className="hero-image-container">
+        <img src={heroSection} alt="Student with books" className="hero-image" />
+      </div>
+      
+      {/* Волнистый фон внизу с помощью SVG */}
+      <svg className="wave" viewBox="0 0 1440 100" preserveAspectRatio="none">
+        <path
+          fill="#fff"
+          d="M0,0 Q720,200 1440,0 L1440,100 L0,100 Z"
+        />
+      </svg>
+    </section>
+  );
+};
+
+export default HeroSection;
--- src/pages/PgMain.jsx
+++ src/pages/PgMain.jsx
@@ -1,11 +1,11 @@
 import Navbar from "../components/Navbar"
-
+import HeroSection from "../components/HeroSection"
 function PgMain() {
 
     return (
       <>
         <Navbar/>
-        
+        <HeroSection/>
       </>
     )
   }
